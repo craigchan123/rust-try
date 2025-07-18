8864ba7be000016d326d3efbe8120367b6e2c580
craigchan123 <craigreus13579@gmail.com>
2025-07-18 18:41:13 +0800

    tasks,launch.json added and configures

---
+++ my_first_rust_code/target/debug/deps/my_first_rust_code.d
@@ -0,0 +1,5 @@
+c:\Users\USER10907\project0717\rust-try\my_first_rust_code\target\debug\deps\my_first_rust_code.d: src\main.rs
+
+c:\Users\USER10907\project0717\rust-try\my_first_rust_code\target\debug\deps\my_first_rust_code.exe: src\main.rs
+
+src\main.rs:
