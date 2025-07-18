86b7f5d924cb053f5eb4013d8eda4285c87e63f6
craigchan123 <craigreus13579@gmail.com>
2025-07-18 19:41:06 +0800

    updated launch and tasks json so that it works when new projects are created

---
+++ trytry/target/debug/deps/trytry.d
@@ -0,0 +1,5 @@
+C:\Users\USER10907\project0717\rust-try\trytry\target\debug\deps\trytry.d: src\main.rs
+
+C:\Users\USER10907\project0717\rust-try\trytry\target\debug\deps\trytry.exe: src\main.rs
+
+src\main.rs:
---
+++ trytry/target/debug/trytry.d
@@ -0,0 +1,1 @@
+C:\Users\USER10907\project0717\rust-try\trytry\target\debug\trytry.exe: C:\Users\USER10907\project0717\rust-try\trytry\src\main.rs
