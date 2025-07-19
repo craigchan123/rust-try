334de8f951ef0f04ce06f99f4a78a4adb55515cc
craigchan123 <craigreus13579@gmail.com>
2025-07-19 11:47:51 +0800

    use of closure and constant function

--- my_first_rust_code/src/main.rs
+++ my_first_rust_code/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
     println!("Hello, world!");
+    let x = 5;
+    let y = 26;
+    let z = x + y;
+    println!("The sum of {} and {} is {}", x, y, z);
+
+    nero()
 }
--- trytry/src/main.rs
+++ trytry/src/main.rs
@@ -1,5 +1,11 @@
-fn main() {
-    println!("Hello, world!");
-    let x = 5;
+const base: i32 = 10;
 
+fn nonnnto (g: i32 , t: i32) -> i32 {
+    g * t
+}
+fn main () {
+    let r = |portionmultiplier|{
+    (base + 9) * portionmultiplier + nonnnto (8 , 6)
+};
+println! ("the result is : {}", r(22));
 }
