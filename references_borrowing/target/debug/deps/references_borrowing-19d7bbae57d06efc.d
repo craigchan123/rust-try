6ed04b1be97bacc0b3b6ff120efff56191bad09d
craigchan123 <151151679+craigchan123@users.noreply.github.com>
2025-07-21 20:30:30 +0800

    references and borrowing file created

---
+++ references_borrowing/target/debug/deps/references_borrowing-19d7bbae57d06efc.d
@@ -0,0 +1,5 @@
+/Users/craig/rust programming/rust-try/references_borrowing/target/debug/deps/references_borrowing-19d7bbae57d06efc.d: src/main.rs
+
+/Users/craig/rust programming/rust-try/references_borrowing/target/debug/deps/libreferences_borrowing-19d7bbae57d06efc.rmeta: src/main.rs
+
+src/main.rs:
---
+++ references_borrowing/target/debug/deps/references_borrowing-ec8dd09fecd718ae.d
@@ -0,0 +1,5 @@
+/Users/craig/rust programming/rust-try/references_borrowing/target/debug/deps/references_borrowing-ec8dd09fecd718ae.d: src/main.rs
+
+/Users/craig/rust programming/rust-try/references_borrowing/target/debug/deps/libreferences_borrowing-ec8dd09fecd718ae.rmeta: src/main.rs
+
+src/main.rs:
